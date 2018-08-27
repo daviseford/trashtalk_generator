ed5120de9251bae5368f196abe98a301f796c965
Davis Ford <davis.e.ford@gmail.com>
2018-08-26 22:35:26 -0400

    moving to the new s3 bucket

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SITE_S3='s3://daviseford-website-code/shittalk/'    # Your S3 bucket address
-SITE_BUILD_DIR='./dist/'                            # Where your site is generated
+SITE_S3='s3://daviseford.com/shittalk/'    # Your S3 bucket address
+SITE_BUILD_DIR='./dist/'    # Where your site is generated
 npm run build
 aws s3 sync --delete --size-only ${SITE_BUILD_DIR} ${SITE_S3} --exclude "*build_log.txt" --exclude "*.idea*" --exclude "*.sh" --exclude "*.git*" --exclude "*.DS_Store"
